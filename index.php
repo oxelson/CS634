8cceca0e02cbc577fb7776920903ad0fcbad7bed
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-11-26 17:54:13 -0700

    renamed to index.xml to .php and modified to use includes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+  <html>
+    <head>
+      <title>Tanya Anisimova</title>
+      <meta name="description" content="Tanya Anisimova - Cellist, Composer, Artist" />
+      <?php include 'head_include.php';?>
+    </head>
+    <body>
+     <?php include 'header.php';?>
+
+         <!-- left side -->
+         <aside class="col-md-4 col-sm-12">          
+          <img src="images/tanya-homepage.png" alt="Photo of Tanya Anisimova by Paul Purpura."/>
+         </aside>
+
+         <!-- right side -->
+         <section class="col-md-8 col-sm-12 homepage">
+           <h1>Tanya Anisimova</h1>
+           <ul>
+            <li>CELLIST</li>
+            <li>COMPOSER</li>
+            <li class="last">ARTIST</li>
+         </section> <!-- /.right -->
+
+  <?php include 'footer.php';?>
