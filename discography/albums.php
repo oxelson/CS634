2580c7d51a36b4b6cd9638dc5c3f4e7244ae1c13
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-11-28 14:38:09 -0700

    added discography section

---
+++ discography/albums.php
@@ -0,0 +1,53 @@
+<!-- DISCOGRAPHY -->
+<!DOCTYPE HTML>
+  <html>
+    <head>
+      <title>Tanya Anisimova : Discography</title>
+      <meta name="description" content="Tanya Anisimova's Discography" />
+      <?php include '../head_include.php';?>
+      <script>
+        jQuery(document).ready(function(){
+          Album.displayAlbums();
+        });
+      </script>
+    </head>
+    <body>
+     <?php include '../header.php';?>
+
+
+       <div class="row subpage">
+          <!-- page title -->
+         <div class="col-sm-4 col-xs-12 left">
+          <h1>Discography</h1>
+         </div>
+
+         <nav class="col-sm-8 col-xs-12 right">
+          <ul>
+            <li class="active">Albums</li>
+            <li><a href="songs.php">Songs</a></li>
+            <li><a href="performances.php">Performances</a></li>
+          </ul>
+         </nav>
+
+       </div> <!-- /.row -->
+
+
+
+       <div class="row subpage">
+         <!-- left side -->
+         <aside class="col-sm-4 col-xs-12">
+          <img src="/images/cello-dark.png" alt="Photo by Omar Khaled from Pexels."/>
+         </aside>
+
+         <!-- right side -->
+         <section class="col-sm-8 col-xs-12">
+           <div class="fill">
+            <ul>
+            </ul>
+           </div>
+         </section> 
+       </div> <!-- /.row -->
+
+      <?php include '../footer.php';?>
+    </body>
+  </html>
---
+++ discography/index.php
@@ -0,0 +1,10 @@
+<!-- DISCOGRAPHY -->
+<!DOCTYPE HTML>
+  <html>
+    <head>
+      <meta http-equiv="refresh" content="0; URL=albums.php">
+      <meta name="keywords" content="automatic redirection">
+    </head>
+    <body>
+    </body>
+  </html>
