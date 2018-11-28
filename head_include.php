35b9e7b15acb9179193099f4af0488597c33dfc5
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-11-28 14:41:42 -0700

    added content population for collection of data

--- head_include.php
+++ head_include.php
@@ -1,15 +1,18 @@
 <?php
 echo "
-      <link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon' />
+      <link rel='shortcut icon' href='/images/favicon.ico' type='image/x-icon' />
       <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta http-equiv='X-UA-Compatible' content='IE=edge' />
       <!-- JS -->
       <script src='https://code.jquery.com/jquery-1.11.3.js'></script>
-      <script src='js/headermenu.js'></script>
+      <script src='/js/Storage.js'></script>
+      <script src='/js/ViewPort.js'></script>
+      <script src='/js/Album.js'></script>
+      <script src='/js/init.js'></script>
 
       <!-- CSS -->
       <link rel='stylesheet' href='https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css'/>
-      <link rel='stylesheet' href='css/main.css' type='text/css' />
+      <link rel='stylesheet' href='/css/main.css' type='text/css' />
 ";
 ?>
