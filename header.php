41b6acd267b42a65fe1b1df8f10af98d41e78ef3
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-11-28 14:40:49 -0700

    move top-level row declaration to content pages

--- header.php
+++ header.php
@@ -9,20 +9,20 @@ echo "
              <ul>
                <li><a href='/news'>NEWS</a></li>
                <li><a href='/calendar'>CALENDAR</a></li>
-               <li><a href='/discographt'>DISCOGRAPHY</a></li>
+               <li><a href='/discography'>DISCOGRAPHY</a></li>
                <li><a href='/about'>ABOUT</a></li>
                <li><a href='/contact'>CONTACT</a></li>
                <li><a href=''>SEARCH</a></li>
              </ul>
              <div class='auth'>
-               <div class='menu'><img src='images/menu.png' alt='Menu'/></div>
+               <div class='menu'><img src='/images/menu.png' alt='Menu'/></div>
                <a href=/login' class='login'>LOGIN</a>
              </div>
            </nav>
              <!-- right end of music bar -->
              <div class='col right'></div>
          </div> <!-- /.row -->
-         <div class='row nobg'>
+         <div class='row nobg hidden'>
            <div id='menu_nav' class='col hidden'><ul></ul></div>
          </div> <!-- /.row -->
        </div> <!-- /.container -->
@@ -30,6 +30,5 @@ echo "
 
    <main>
      <div class='container'>
-       <div class='row'>
 ";
 ?>
