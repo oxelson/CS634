de1834b9521854a3b34bf7191bf7112610e3d78b
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-11-28 14:40:37 -0700

    move top-level row declaration to content pages

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <?php
 echo "
 
-       </div> <!-- /.row -->
      </div> <!-- /.container -->
    </main>
 
@@ -10,29 +9,29 @@ echo "
        <div class='row'>
          <!-- copyright -->
          <div class='col left'>
-           Copyright &copy;2014-2018 Tanya Anisimova &bull; All Rights Reserved
+           &copy; 2014-2018 Tanya Anisimova &nbsp; &bull; &nbsp; <a href='/sources.php'>Sources</a>
          </div> <!-- /.left -->
 
          <!-- social media icons -->
          <div class='col right'>
            <ul>
              <li>
-               <a href='https://itunes.apple.com/us/artist/tanya-anisimova/id47602083'><img src='images/apple-white.png' alt='Purchase Tanya's music from Apple iTunes'/></a>
+               <a href='https://itunes.apple.com/us/artist/tanya-anisimova/id47602083'><img src='/images/apple-white.png' alt='Purchase Tanya's music from Apple iTunes'/></a>
              </li>
              <li>
-               <a href='https://www.amazon.com/s/ref=sr_nr_n_4?rh=n:163856011,k:tanya+anisimova&keywords=tanya+anisimova'><img src='images/amazonsmile-white.png' alt='Purchase Tanya's music from Amazon Smile'/></a>
+               <a href='https://www.amazon.com/s/ref=sr_nr_n_4?rh=n:163856011,k:tanya+anisimova&keywords=tanya+anisimova'><img src='/images/amazonsmile-white.png' alt='Purchase Tanya's music from Amazon Smile'/></a>
              </li>
              <li>
-               <a href='https://open.spotify.com/artist/5XmzcguryovRXLUzEkBACB'><img src='images/spotify-white.png' alt='Listen to Tanya's music on Spotify'/></a>
+               <a href='https://open.spotify.com/artist/5XmzcguryovRXLUzEkBACB'><img src='/images/spotify-white.png' alt='Listen to Tanya's music on Spotify'/></a>
              </li>
              <li>
-               <a href='https://vimeo.com/search?q=tanya-anisimova'><img src='images/vimeo-white.png' alt='Watch Tanya's performances on Vimeo'/></a>
+               <a href='https://vimeo.com/search?q=tanya-anisimova'><img src='/images/vimeo-white.png' alt='Watch Tanya's performances on Vimeo'/></a>
              </li>
              <li>
-               <a href='https://www.facebook.com/TANYAANISIMOVAA'><img src='images/facebook-white.png' alt='Follow Tanya on Facebook'/></a>
+               <a href='https://www.facebook.com/TANYAANISIMOVAA'><img src='/images/facebook-white.png' alt='Follow Tanya on Facebook'/></a>
              </li>
              <li>
-               <a href='https://www.youtube.com/channel/UCXa0NSwoFPPOeWIkVbaTeCQ'><img src='images/youtube-white.png' alt='Watch Tanya's performances on YouTube'/></a>
+               <a href='https://www.youtube.com/channel/UCXa0NSwoFPPOeWIkVbaTeCQ'><img src='/images/youtube-white.png' alt='Watch Tanya's performances on YouTube'/></a>
              </li>
            </ul>
          </div> <!-- /.right -->
