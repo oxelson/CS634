c3aef91d6eabfe3c67cb69a09f1502d53c53da0b
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-12-03 19:45:06 -0700

    sources files

--- sources.php
+++ sources.php
@@ -4,10 +4,10 @@
     <head>
       <title>Tanya Anisimova : Sources</title>
       <meta name="description" content="Sources used for Tanya Anisimova's Website" />
-      <?php include '../head_include.php';?>
+      <?php include 'head_include.php';?>
     </head>
     <body>
-     <?php include '../header.php';?>
+     <?php include 'header.php';?>
 
 
        <div class="row subpage">
@@ -34,17 +34,40 @@
            <div class="fill">
              <div class="row">
                <div class="col">
-                 <h3>Curriculum Vitae</h3>
-By Peter K Burian - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=70163494
-https://www.musicdurham.co.uk/venue/ushaw-college/
+                 <h3>Sources & References For Website</h3>
 
-https://commons.wikimedia.org/wiki/File:St_James_Church_Piccadilly_Interior.jpg
-Lonpicman at English Wikipedia
+                 <h5>Image Credits</h5>
+                 <ul class="realList">
 
-http://www.promisek.org/Programs.aspx
+                   <li><a href="/images/floral-background.png">Corner Floral Frame</a> sourced from <a href="https://www.publicdomainpictures.net/en/view-image.php?image=65562&picture=floral-corner-frame">Public Domain Pictures</a>.</li>
+                   <li><a href="/images/cello-dark.png">Subpage image of Tanya</a> sourced from <a href="https://www.pexels.com/photo/black-classic-music-violin-861884/">Omar Khaled via Pexels</a>.</li>
+                   <li><a href="/images/tanya-homepage.png">Homepage image of Tanya</a> sourced from <a href="http://www.tanyaanisimova.com/">Paul Purpura via Tanya Anisimova's website</a>.</li>
+                   <li><a href="/images/st_andrews.png">St Andrew's image</a> sourced from <a href="https://commons.wikimedia.org/w/index.php?curid=70163494">Peter K Burian via Wikimedia Commons</a>.</li>
+                   <li><a href="/images/ushaw.png">Ushaw image</a> sourced from <a href="https://www.musicdurham.co.uk/venue/ushaw-college/">Music in Durham website</a>.</li>
+                   <li><a href="/images/st_james.png">St James</a> sourced from <a href="https://commons.wikimedia.org/wiki/File:St_James_Church_Piccadilly_Interior.jpg">lonpicman via Wikimedia Commons</a>.</li>
+                   <li><a href="/images/promisek.png">Promisek image</a> sourced from <a href="http://www.promisek.org/Programs.aspx">Promisek at Three Rivers website</a>.</li>
+                   <li><a href="/images/anufriev-rouault.png">Rouault image</a> sourced from <a href="http://www.tanyaanisimova.com/">Alexander Anufriev via Tanya Anisimova's website</a>.</li>
+                   <li><a href="/images/arts_on_the_green.png">Arts on the Green image </a> sourced from <a href="https://www.artsonthegreen.info/"> Arts on the Green website</a>.</li>
+                   <li><a href="/images/esgf.png">Eastern Shore Guitar Festival image </a> sourced from <a href="https://www.washcoll.edu/departments/music/eastern-shore-guitar-festival/">Eastern Shore Guitar Festival and Workshop website</a>.</li>
+                   <li><a href="/images/loyola.png">Loyola image </a> sourced from <a href="https://www.luc.edu/">Loyola  website</a>.</li>
+                   <li>All other images of Tanya and albums sourced from <a href="http://www.tanyaanisimova.com/">Tanya Anisimova's website</a>.</li>
+                 </ul>
 
- <img src="/images/tanya-homepage.png" alt="Photo of Tanya Anisimova by Paul Purpura."/>
+                 <h5>Content Sources</h5>
+                 <p>Content for this website was taken from the following sources:</p>
+                 <ul class="realList">
+                   <li> <a href="http://www.tanyaanisimova.com/">Tanya Anisimova's website</a>.</li>
+                   <li> <a href="https://itunes.apple.com/us/artist/tanya-anisimova/id47602083">Tanya Anisimova's iTunes Store</a>.</li>
+                   <li> <a href="https://www.amazon.com/s/ref=sr_nr_n_4?rh=n:163856011,k:tanya+anisimova&keywords=tanya+anisimova">Tanya Anisimova's Amazon Smile Store</a>.</li>
+                 </ul>
+                 
 
+                 <h5>Sources of Inspiration</h5>
+
+                 <ul class="realList">
+                   <li> <a href="http://joshuabell.com/">Joshua Bell's website</a>.</li>
+                   <li> <a href="https://www.yo-yoma.com/">Yo-Yo Ma's website</a>.</li>
+                 </ul>
 
                </div> <!--/.col -->
              <div> <!--/.row -->
@@ -52,6 +75,6 @@ http://www.promisek.org/Programs.aspx
          </section>
        </div> <!-- /.row -->
 
-      <?php include '../footer.php';?>
+      <?php include 'footer.php';?>
     </body>
   </html>
